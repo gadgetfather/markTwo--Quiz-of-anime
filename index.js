e40aba5aecebafb325fe53398729fd6d7d660504
Akshay Kamble <77927918+gadgetfather@users.noreply.github.com>
2021-09-02 10:35:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var readlineSync = require('readline-sync');
+const chalk = require('chalk');
+
+var score =0
+var name= readlineSync.question(chalk.green("What is your name "))
+console.log(chalk.magenta("Welcome "+ name+ " To ****ANIME QUIZ ***"))
+
+
+
+var scores= [{
+  name:"Akshay",
+  score:4
+},{
+  name:"Aditya",
+  score:3
+}]
+
+
+function play(question,answer){
+var userAnswer = readlineSync.question(chalk.yellow(question))
+if (userAnswer.isUpperCase===answer.isUpperCase) {
+  console.log(chalk.greenBright("right!"))
+  score++
+ 
+}
+else {
+  console.log(chalk.redBright("wrong!"))
+}
+ console.log(chalk.blueBright("Your score is ",score))
+}
+
+var questions = [{
+  question:"Who is main character in Dragon Ball Z? ",
+  answer:"Goku"
+},{
+  question:"In which anime note book is used to kill people? ",
+  answer:"Death note" 
+},{
+  question:"what is the name of one punch man? ",
+  answer:"Saitama"
+}, {
+  question:"Who was number one hero in my hero academia? ",
+  answer:"All might"},{
+  question:"What is the name of fox inside naruto? ",
+  answer:"Kurama"}]
+
+  for(i=0; i<questions.length;i++){
+    var currenQuestion = questions[i]
+    play(currenQuestion.question,currenQuestion.answer)
+  }
+
+  console.log(chalk.blueBright("WOW! You scored ",score," points"))
+
+  console.log(chalk.red("*** check out highScores ***"))
+  
+  for(i=0;i<scores.length;i++)
+  {
+  console.log(scores[i].name,scores[i].score)
+  }
